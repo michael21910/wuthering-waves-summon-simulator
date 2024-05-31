9665d327c7197a7062d6d5093c0320ab1b2bd2a8
michael21910 <faefae31910@gmail.com>
2024-05-31 17:07:02 +0800

    [ 2024/05/31 ] [ Better Coding Style ]

--- index.py
+++ index.py
@@ -37,7 +37,7 @@ def runOne(X, Y):
             pity4Count, pity5Count, fourCount = getFourStar(pity4Count, pity5Count, fourCount)
             continue
         
-        summon = round(random.uniform(0, 1), 6) * 100
+        summon = round(random.uniform(0, 1), 3) * 100
         # summon (original rate)
         if pity5Count <= X:
             if 0 <= summon and summon <= 0.8:
@@ -66,37 +66,50 @@ def runOne(X, Y):
                 pity4Count, pity5Count, threeCount = getThreeStar(pity4Count, pity5Count, threeCount)
     return output, threeCount, fourCount, fiveCount
 
-def SetXYPairLinear():
+def SetXYPairLinear(testFrom, testTo):
     # generate X, Y pair
     X_values = []
     Y_values = []
     initial = 0.8
     final = 100
-    testFrom = 40
-    testTo = 76
     for i in range(testFrom, testTo):
         gap = 80 - i
         X_values.append(i)
         Y_values.append((final - initial) / gap)
     return X_values, Y_values, testFrom, testTo
 
-def SetXYPairMIHOYO():
+def SetXYPairMIHOYO(testFrom, testTo):
     # generate X, Y pair
     X_values = []
     Y_values = []
-    testFrom = 40
-    testTo = 76
     for i in range(testFrom, testTo):
         X_values.append(i)
         Y_values.append(8)
     return X_values, Y_values, testFrom, testTo
 
+def drawLineChart(X_values, ratio, officalRatio, mode, starLevel):
+    plt.figure()
+    plt.plot(X_values, ratio, color='r', marker='o')
+    plt.plot(X_values, officalRatio, color='b', marker='o')
+    plt.xlabel("X")
+    plt.ylabel(f"{starLevel}-star summon ratio (%)")
+    plt.title(f"{starLevel}-star summon ratio")
+    plt.savefig(f"./{mode.lower()}/{starLevel}-star summon ratio {mode}.png")
+    plt.close()
+
+def drawBarChart(X_values, ratio, mode, index):
+    plt.figure()
+    plt.bar(range(1, 81), ratio, color='b')
+    plt.xlabel("Summon")
+    plt.ylabel("5-star summon ratio (%)")
+    plt.title("5-star summon ratio per summon")
+    plt.savefig(f"./{mode.lower()}/5-star ratio per summon {mode} X={X_values[index]}.png")
+    plt.close()
+
 def GetAnswer(X_values, Y_values, testFrom, testTo, mode):
     assert mode == "linear" or mode == "miHoYo", "mode should be either 'linear' or 'miHoYo'"
 
-    threeStarRatio = list()
-    fourStarRatio = list()
-    fiveStarRatio = list()
+    threeStarRatio, fourStarRatio, fiveStarRatio = list(), list(), list()
     threeStarOfficalRatio = [86.2] * (testTo - testFrom)
     fourStarOfficalRatio = [12] * (testTo - testFrom)
     fiveStarOfficalRatio = [1.8] * (testTo - testFrom)
@@ -104,11 +117,7 @@ def GetAnswer(X_values, Y_values, testFrom, testTo, mode):
     for i in range(len(X_values)):
         X = X_values[i]
         Y = Y_values[i]
-        totalThree = 0
-        totalFour = 0
-        totalFive = 0
-        totalThreePerSummon = [0] * 80
-        totalFourPerSummon = [0] * 80
+        totalThree, totalFour, totalFive = 0, 0, 0
         totalFivePerSummon = [0] * 80
         runCount = 100000
         for _ in range(runCount):
@@ -117,66 +126,34 @@ def GetAnswer(X_values, Y_values, testFrom, testTo, mode):
             totalFour += fourCount
             totalFive += fiveCount
             for j in range(0, 80):
-                if output[j] == '3':
-                    totalThreePerSummon[j] += 1
-                elif output[j] == '4':
-                    totalFourPerSummon[j] += 1
-                elif output[j] == '5':
+                if output[j] == '5':
                     totalFivePerSummon[j] += 1
-        # totalThreePerSummon = [x * 100 / runCount / 80 for x in totalThreePerSummon]
-        # totalFourPerSummon = [x * 100 / runCount / 80 for x in totalFourPerSummon]
-        totalFivePerSummon = [x * 100 * 100 / runCount / 80 for x in totalFivePerSummon]
-        print(f"X={X}, Y={Y}%")
-        print(f"3-star: {totalThree * 100 / runCount / 80}%")
-        print(f"4-star: {totalFour * 100 / runCount / 80}%")
-        print(f"5-star: {totalFive * 100 / runCount / 80}%")
+        totalFivePerSummon = [x * 10000 / runCount / 80 for x in totalFivePerSummon]
+        writingResults = f"X = {X}, Y = {Y}%\n\
+3-star: {totalThree * 100 / runCount / 80}%\n\
+4-star: {totalFour * 100 / runCount / 80}%\n\
+5-star: {totalFive * 100 / runCount / 80}%\n\n"
+
+        with open(f"./{mode.lower()}/{mode.lower()}.txt", "a") as f:
+            f.write(writingResults)
+
         threeStarRatio.append(totalThree * 100 / runCount / 80)
         fourStarRatio.append(totalFour * 100 / runCount / 80)
         fiveStarRatio.append(totalFive * 100 / runCount / 80)
-        print("X =", X_values[i], "finished!\n")
 
         # five star ratio per summon
-        plt.figure()
-        plt.bar(range(1, 81), totalFivePerSummon, color='b')
-        plt.xlabel("Summon")
-        plt.ylabel("5-star summon ratio (%)")
-        plt.title("5-star summon ratio per summon")
-        plt.savefig(f"./{mode.lower()}/5-star ratio per summon {mode} X={X_values[i]}.png")
-        plt.close()
+        drawBarChart(X_values, totalFivePerSummon, mode, i)
 
-    # three star ratio
-    plt.figure()
-    plt.plot(X_values, threeStarRatio, color='r', marker='o')
-    plt.plot(X_values, threeStarOfficalRatio, color='b', marker='o')
-    plt.xlabel("X")
-    plt.ylabel("3-star summon ratio (%)")
-    plt.title("3-star summon ratio")
-    plt.savefig(f"./{mode.lower()}/3-star summon ratio {mode}.png")
-    plt.close()
+        print("mode:", mode, "| X:", X, "finished!")
 
-    # four star ratio
-    plt.figure()
-    plt.plot(X_values, fourStarRatio, color='r', marker='o')
-    plt.plot(X_values, fourStarOfficalRatio, color='b', marker='o')
-    plt.xlabel("X")
-    plt.ylabel("4-star summon ratio (%)")
-    plt.title("4-star summon ratio")
-    plt.savefig(f"./{mode.lower()}/4-star summon ratio {mode}.png")
-    plt.close()
-
-    # five star ratio
-    plt.figure()
-    plt.plot(X_values, fiveStarRatio, color='r', marker='o')
-    plt.plot(X_values, fiveStarOfficalRatio, color='b', marker='o')
-    plt.xlabel("X")
-    plt.ylabel("5-star summon ratio (%)")
-    plt.title("5-star summon ratio")
-    plt.savefig(f"./{mode.lower()}/5-star summon ratio {mode}.png")
-    plt.close()
+    # star ratio for 3, 4, 5 stars
+    drawLineChart(X_values, threeStarRatio, threeStarOfficalRatio, mode, 3)
+    drawLineChart(X_values, fourStarRatio, fourStarOfficalRatio, mode, 4)
+    drawLineChart(X_values, fiveStarRatio, fiveStarOfficalRatio, mode, 5)
 
 if __name__ == "__main__":
-    X_values, Y_values, testFrom, testTo = SetXYPairLinear()
+    X_values, Y_values, testFrom, testTo = SetXYPairLinear(40, 76)
     GetAnswer(X_values, Y_values, testFrom, testTo, mode='linear')
 
-    X_values, Y_values, testFrom, testTo = SetXYPairMIHOYO()
+    X_values, Y_values, testFrom, testTo = SetXYPairMIHOYO(40, 76)
     GetAnswer(X_values, Y_values, testFrom, testTo, mode='miHoYo')
